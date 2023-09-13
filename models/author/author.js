8777e5e0b6474ac9b8fdd9613f5908785d95001a
egemenatikk <egemenatikk@gmail.com>
2023-09-13 23:10:42 +0300

    Rearranged folder structure under models directory

--- models/author/author.js
+++ models/author/author.js
@@ -33,6 +33,7 @@ const authorSchema = new Schema({
 {
     timestamps: true,
     toJSON: {
+        getters: true,
         transform: function (doc, ret, options) {
             ret.id = ret._id;
             delete ret._id;
